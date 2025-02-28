9ea33c1fa6abf2b29ea720a28a9c2eb3e48befc8
Luciano Raspo <lucianoraspo@outlook.com>
2025-02-28 20:57:19 -0300

    Final Proyect in process

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "Example Review", "isbn": "9783161484100"},
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "Example Review 2", "isbn": "9780596520687"},
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": "Example Review 3", "isbn": "9781402894626"},
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": "Example Review 4", "isbn": "9780545010221"},
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": "Example Review 5", "isbn": "9781861978769"},
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": "Example Review 6", "isbn": "9780307277784"},
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": "Example Review 7", "isbn": "9780743273565"},
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": "Example Review 8", "isbn": "9781250307469"},
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": "Example Review 9", "isbn": "9780062316097"},
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": "Example Review 10", "isbn": "1234567890"}
 }
 
 module.exports=books;
