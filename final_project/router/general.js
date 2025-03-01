a89dc899caaf9be7928b4ab426e9d2ee5cb275fe
Luciano Raspo <lucianoraspo@outlook.com>
2025-03-01 16:03:46 -0300

    Final Project Finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -87,3 +87,72 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+
+/* AXIOS - ASYNC - AWAIT
+
+let books = require("./booksdb.js");
+
+//USED AXIOS
+
+const axios = require('axios');
+
+//GET ALL BOOKS
+
+const getBooks = async () => {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        console.log("List Books:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error view books:", error.message);
+    }
+  };
+  
+  getBooks();
+
+//GET BOOK FOR ISBN
+
+  const getBookByIsbn = async (isbn) => {
+      try {
+          const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+          console.log("Book Found By ISBN:");
+          console.log(response.data);
+      } catch (error) {
+          console.error("Error view book", error.message);
+      }
+  };
+  
+
+  getBookByIsbn("9783161484100"); // ISBN EXAMPLE
+
+//GET BOOK FOR AUTHOR
+
+  const getBookByAuthor = async (author) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        console.log("Book Found By Author:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error view book/s", error.message);
+    }
+};
+
+
+getBookByAuthor("Hans Christian Andersen");// AUTHOR EXAMPLE
+  
+
+//GET BOOK FOR TITLE
+
+const getBookByTitle = async (title) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        console.log("Book Found By Title:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error view book/s", error.message);
+    }
+};
+
+
+getBookByTitle("The Epic Of Gilgamesh");// TITLE EXAMPLE
+ */
--- final_project/router/generalAxios.js
+++ final_project/router/generalAxios.js
@@ -1,7 +1,10 @@
-//USED AXIOS
 let books = require("./booksdb.js");
 
+//USED AXIOS
+
 const axios = require('axios');
+
+//GET ALL BOOKS
 
 const getBooks = async () => {
     try {
@@ -14,3 +17,46 @@ const getBooks = async () => {
   };
   
   getBooks();
+
+//GET BOOK FOR ISBN
+
+  const getBookByIsbn = async (isbn) => {
+      try {
+          const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+          console.log("Book Found By ISBN:");
+          console.log(response.data);
+      } catch (error) {
+          console.error("Error view book", error.message);
+      }
+  };
+  
+  getBookByIsbn("9783161484100"); // ISBN EXAMPLE
+
+//GET BOOK FOR AUTHOR
+
+  const getBookByAuthor = async (author) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        console.log("Book Found By Author:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error view book/s", error.message);
+    }
+};
+
+getBookByAuthor("Hans Christian Andersen");// AUTHOR EXAMPLE
+  
+
+//GET BOOK FOR TITLE
+
+const getBookByTitle = async (title) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        console.log("Book Found By Title:");
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error view book/s", error.message);
+    }
+};
+
+getBookByTitle("The Epic Of Gilgamesh");// TITLE EXAMPLE
